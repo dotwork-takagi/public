55820a9e37973fb8a3fae0f8074afb5513c2f24a
dotwork-takagi <dotwork.takagi@gmail.com>
2024-09-24 11:40:34 +0900

    a

--- dotty_games/js/main.js
+++ dotty_games/js/main.js
@@ -9,7 +9,7 @@ function SetBgcolor(color) { // 背景色を変える
     document.body.style.backgroundColor = color
 }
 
-function getRandomInt(min, max) {
+function getRandomInt(min, max) { // ランダムな整数を取得する
     min = Math.ceil(min)
     max = Math.floor(max)
     return Math.floor(Math.random() * (max - min + 1)) + min
